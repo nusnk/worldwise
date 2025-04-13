abf129b780f71727722e0593d877c2af3049e12b
Nurbolat Sansyzbay <nurbolatsnk@gmail.com>
2025-04-13 19:10:45 +0500

    Moved cities retrieval logic to context

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,7 +3,7 @@ import styles from "./Map.module.css";
 
 export default function Map() {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
