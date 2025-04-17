883a602b61b990e753fa49bfeec390a1eaa69339
Nurbolat Sansyzbay <nurbolatsnk@gmail.com>
2025-04-17 10:41:29 +0500

    Finished city view

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router";
+import Button from "./Button";
+
+export default function BackButton() {
+  const navigate = useNavigate();
+  return (
+     <Button type="back" onClick={(e) => {
+      e.preventDefault();
+      navigate(-1);
+    }}>&larr; Back</Button>
+  )
+}
