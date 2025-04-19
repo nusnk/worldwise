e6622afe1e9a8f946a66f77301eeebdc10a83b6a
Nurbolat Sansyzbay <nurbolatsnk@gmail.com>
2025-04-19 22:14:50 +0500

    Installed leaflet and react-leaflet and added map container

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
