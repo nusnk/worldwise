4d27264c9a3969fdbb14b50c8983cc6321467e3d
Nurbolat Sansyzbay <nurbolatsnk@gmail.com>
2025-04-20 18:18:00 +0500

    Added click detection, and change centering logic to a map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,23 @@
 import { useNavigate, useSearchParams } from "react-router";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 export default function Map() {
-  const navigate = useNavigate();
+  
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const { cities } = useCities();
   const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-
+  useEffect(function() {
+    if (!lat || !lng) return;
+    setMapPosition([lat || 40, lng || 0]);
+  }, [lat, lng])
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+    <div className={styles.mapContainer} >
+      <MapContainer zoom={13} scrollWheelZoom={true} className={styles.map}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -26,7 +29,23 @@ export default function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition}/>
+        <DetectClick />
       </MapContainer>
     </div>
   )
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setZoom(6);
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+   useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+   })
 }
